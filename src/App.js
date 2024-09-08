6009e7aaa0d9b4a37aba3ff6ac5cbbb5c5f2d515
Pharmamara <140740925+Pharmamara@users.noreply.github.com>
2024-09-08 18:45:46 +0300

    routes and header add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import words from "./components/Dictionary";
+import Header from "./components/header/header";
+import Error404 from "./components/error404/error404";
 import Slider from "./components/slider/slider";
 import WordList from "./components/wordList/wordList";
-import WordTable from "./components/wordTable/wordTable";
+import Training from "./components/training/training";
+import style from "./App.module.css";
 
 function App() {
   return (
     <>
-      <Slider />
-      <WordList />
-      <WordTable />
+      <div className={style.App}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<WordList />}></Route>
+          <Route path="/wordTable" element={<WordList />}>
+            Список слов
+          </Route>
+          <Route path="/wordCards" element={<Slider words={words} />}>
+            Карточки слов
+          </Route>
+          <Route path="/wordTraining" element={<Training />}>
+            Карточки слов
+          </Route>
+          <Route path="*" element={<Error404 />}></Route>
+        </Routes>
+      </div>
     </>
   );
 }
+
 export default App;
--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,53 +1,57 @@
-import React from 'react'
-import WordCard from '../wordCard/WordCard'
-import { words } from "../Dictionary"
-import style from './slider.module.css'
+import React from "react";
+import WordCard from "../wordCard/WordCard";
+import { words } from "../Dictionary";
+import ArrowLeft from "./arrow_left.svg";
+import ArrowRight from "./arrow_right.svg";
+import style from "./slider.module.css";
 
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function Slider() {
-    
-    const [pressed, setPressed] = useState(false)
-    const [cardIndex, setCardIndex] = useState(0)
-    
+  const [pressed, setPressed] = useState(false);
+  const [cardIndex, setCardIndex] = useState(0);
 
-    const showPrevCard = () => {
-        let index = cardIndex;
+  const showPrevCard = () => {
+    let index = cardIndex;
 
-        if (index !== 0) {
-            index--;
-            setCardIndex(index);
-        } else if (index === 0) {
-            setCardIndex(0);
-        }
-        setPressed(false)
+    if (index !== 0) {
+      index--;
+      setCardIndex(index);
+    } else if (index === 0) {
+      setCardIndex(0);
     }
+    setPressed(false);
+  };
 
-    const showNextCard = () => {
-        let index = cardIndex;
+  const showNextCard = () => {
+    let index = cardIndex;
 
-        if (index !== words.length - 1) {
-            index++;
-            setCardIndex(index);
-        } else if (index === words.length - 1) {
-            setCardIndex(words.length - 1);
-        }
-        setPressed(false)
+    if (index !== words.length - 1) {
+      index++;
+      setCardIndex(index);
+    } else if (index === words.length - 1) {
+      setCardIndex(words.length - 1);
     }
+    setPressed(false);
+  };
 
-    
-    return (
-<div className={style.main}>
-            <div className={style.container}>
-                <button onClick={showPrevCard} className={style.prev_btn}><img src="./assets/arrow_left.svg" alt="" className={style.nav_btn} /></button>
-                <WordCard
-                    word={words[cardIndex].english}
-                    transcription={words[cardIndex].transcription}
-                    translate={words[cardIndex].russian}
-                    pressed={pressed}
-                    setPressed={setPressed} />
-                <button onClick={showNextCard} className={style.next_btn}><img src="./assets/arrow_right.svg" alt="" className={style.nav_btn} /></button>
-            </div >
-        </div >
-    )
+  return (
+    <div className={style.main}>
+      <div className={style.container}>
+        <button onClick={showPrevCard} className={style.prev_btn}>
+          <img src={ArrowLeft} alt="" className={style.nav_btn} />
+        </button>
+        <WordCard
+          word={words[cardIndex].english}
+          transcription={words[cardIndex].transcription}
+          translate={words[cardIndex].russian}
+          pressed={pressed}
+          setPressed={setPressed}
+        />
+        <button onClick={showNextCard} className={style.next_btn}>
+          <img src={ArrowRight} alt="" className={style.nav_btn} />
+        </button>
+      </div>
+    </div>
+  );
 }
