4778d1cc060674759647ae515edea3dc4462825f
Pharmamara <140740925+Pharmamara@users.noreply.github.com>
2024-09-21 17:05:29 +0300

    focus btnTranslate add

--- src/components/btnTranslate/btnTranslate.jsx
+++ src/components/btnTranslate/btnTranslate.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import style from './btnTranslate.module.css'
+import React from "react";
+import { useRef, useEffect } from "react";
+import style from "./btnTranslate.module.css";
 
 export default function BtnTranslate(props) {
-    const handleClick = () => {
-        props.setPressed(!props.pressed);
-        props.countWords();
-        
-    };
-    return (
-        <div className={style.btn}>
-            {props.pressed ? <span className={style.translate}>{props.translate}</span> :
-            <button className={style.btn} onClick={handleClick}>показать перевод</button>}
-        </div>
-    )
+  const btnReference = useRef(null);
+  useEffect(() => {
+    if (btnReference.current) {
+      btnReference.current.focus();
+    }
+  }, []);
+
+  const handleClick = () => {
+    props.setPressed(!props.pressed);
+    props.countWords();
+  };
+  return (
+    <div className={style.btn}>
+      {props.pressed ? (
+        <span className={style.translate}>{props.translate}</span>
+      ) : (
+        <button ref={btnReference} className={style.btn} onClick={handleClick}>
+          показать перевод
+        </button>
+      )}
+    </div>
+  );
 }
